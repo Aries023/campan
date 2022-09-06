14b642f0317d2da7d77978a8a130fc96ec9e2ac6
Aries023 <pet.potocka@gmail.com>
2022-09-06 22:21:22 +0200

    - repair FXMLLoader (renamed mainCampan.fxml)

--- src/main/java/com/example/campan/Campan.java
+++ src/main/java/com/example/campan/Campan.java
@@ -10,7 +10,7 @@ import java.io.IOException;
 public class Campan extends Application {
     @Override
     public void start(Stage stage) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(Campan.class.getResource("hello-view.fxml"));
+        FXMLLoader fxmlLoader = new FXMLLoader(Campan.class.getResource("mainCampan.fxml"));
         Scene scene = new Scene(fxmlLoader.load(), 320, 240);
         stage.setTitle("Campan wrap party!");
         stage.setScene(scene);
