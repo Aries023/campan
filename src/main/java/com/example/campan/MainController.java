1ef940ba8a4f575b585c989768fc9133ab909331
Aries023 <pet.potocka@gmail.com>
2022-09-08 13:15:59 +0200

    - add some buttons actions

--- src/main/java/com/example/campan/MainController.java
+++ src/main/java/com/example/campan/MainController.java
@@ -5,11 +5,11 @@ import javafx.scene.control.Label;
 
 public class MainController {
     @FXML
-    private Label topPanelIngr;
+    private Label topPanelIngr; // top label for last used ingredient
     @FXML
-    private Label botPanelWrap;
+    private Label botPanelWrap; // bot label for kind of wrap
 
-    // left menu buttons but also secret button on top bar
+    // left menu --> buttons
     @FXML
     protected void baconButtonMenu() {
         botPanelWrap.setText("bacon wrap");
@@ -29,6 +29,41 @@ public class MainController {
     @FXML
     protected void randomButtonMenu() {
         botPanelWrap.setText("Welcome to JavaFX Application!");
+    }
+
+    // center --> garnir buttons
+
+    @FXML
+    protected void garnir0Ajvar() {
+        topPanelIngr.setText("Ajvar sos");
+    }
+    @FXML
+    protected void garnir0Asia() {
+        topPanelIngr.setText("Asia sos");
+    }
+    @FXML
+    protected void garnir0Horcica() {
+        topPanelIngr.setText("Hrubozrna horčica");
+    }
+    @FXML
+    protected void garnir0Tasty() {
+        topPanelIngr.setText("Tasty sos");
+    }
+    @FXML
+    protected void garnir0Italian() {
+        topPanelIngr.setText("Tomato Italian sos");
+    }
+    @FXML
+    protected void garnir0Garlic() {
+        topPanelIngr.setText("Garlic sos");
+    }
+    @FXML
+    protected void garnir0Sandwitch() {
+        topPanelIngr.setText("Sandwitch sos");
+    }
+    @FXML
+    protected void garnir0BigMac() {
+        topPanelIngr.setText("Big Mac sos");
     }
 
 }
