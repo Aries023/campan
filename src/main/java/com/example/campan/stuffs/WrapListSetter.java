73879f056c3adb99084adf578660bc06e5d5f8d6
Aries023 <pet.potocka@gmail.com>
2022-09-12 01:33:09 +0200

    - add 3 kinds of wraps in WrapListSetter

--- src/main/java/com/example/campan/stuffs/WrapListSetter.java
+++ src/main/java/com/example/campan/stuffs/WrapListSetter.java
@@ -16,4 +16,39 @@ public class WrapListSetter { // set wrap pole to bacon wrap
         pole.add("chedar");
         pole.add("bacon");
     }
+    public static void setToItalianWrap(List<String> pole){
+        // clear pole
+        pole.clear();
+
+        // add ingrediends
+        pole.add("garlic");
+        pole.add("tomato");
+        pole.add("cibulaB");
+        pole.add("rukola");
+        pole.add("paradajky");
+        pole.add("uhorkyS");
+    }
+    public static void setToAsiaWrap(List<String> pole){
+        // clear pole
+        pole.clear();
+
+        // add ingrediends
+        pole.add("garlic");
+        pole.add("sriracha");
+        pole.add("cibulaR");
+        pole.add("salat");
+        pole.add("uhorkyS");
+    }
+    public static void setToAjvarWrap(List<String> pole){
+        // clear pole
+        pole.clear();
+
+        // add ingrediends
+        pole.add("sandwitch");
+        pole.add("ajvar");
+        pole.add("kapia");
+        pole.add("salat");
+        pole.add("rukola");
+        pole.add("paradajky");
+    }
 }
