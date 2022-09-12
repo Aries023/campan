6d7986ebb15b38586b95fe4c3bb9612741bfe7b9
Aries023 <pet.potocka@gmail.com>
2022-09-12 14:32:15 +0200

    - add function for buttons (garnir2 and garnir3) (only random button in menu left) !I found finisher problem and I have to fix it in next update!

--- src/main/java/com/example/campan/MainController.java
+++ src/main/java/com/example/campan/MainController.java
@@ -368,4 +368,212 @@ public class MainController {
         }
     }
 
+    // garnir 2
+    public void garnir2Cheddar() {
+        if (Corrector.nullChecker(actualWrap)){ // check if its null
+            topPanelIngr.setText("Ups.. a aky že wrap to robime?");
+        }else {
+            if (Corrector.controlIngredient(actualWrap,position,"cheddar")){ // check if its correct ingredient
+                if (Corrector.finishControl(actualWrap,position)){
+                    topPanelIngr.setText("Congratulations, you did it!");
+                    position=0;
+                }else {
+                    topPanelIngr.setText("Cheddar");
+                    position++;
+                }
+            }else {
+                if (Corrector.isContains(actualWrap,"cheddar")){ // if its not correct then if wrap contain the ingredient
+                    topPanelIngr.setText("niečo si preskočil alebo tam toho dal trocha vela.. ale vratim  čas a dam ti este šancu..");
+                }else {
+                    topPanelIngr.setText("Ups.. pokazil si wrap, musiš ist znova!");
+                    position=0;
+                }
+            }
+        }
+    }
+
+    public void garnir2Bacon() {
+        if (Corrector.nullChecker(actualWrap)){ // check if its null
+            topPanelIngr.setText("Ups.. a aky že wrap to robime?");
+        }else {
+            if (Corrector.controlIngredient(actualWrap,position,"bacon")){ // check if its correct ingredient
+                if (Corrector.finishControl(actualWrap,position)){
+                    topPanelIngr.setText("Congratulations, you did it!");
+                    position=0;
+                }else {
+                    topPanelIngr.setText("Bacon");
+                    position++;
+                }
+            }else {
+                if (Corrector.isContains(actualWrap,"bacon")){ // if its not correct then if wrap contain the ingredient
+                    topPanelIngr.setText("niečo si preskočil alebo tam toho dal trocha vela.. ale vratim  čas a dam ti este šancu..");
+                }else {
+                    topPanelIngr.setText("Ups.. pokazil si wrap, musiš ist znova!");
+                    position=0;
+                }
+            }
+        }
+    }
+
+    public void garnir2Paradajky() {
+        if (Corrector.nullChecker(actualWrap)){ // check if its null
+            topPanelIngr.setText("Ups.. a aky že wrap to robime?");
+        }else {
+            if (Corrector.controlIngredient(actualWrap,position,"paradajky")){ // check if its correct ingredient
+                if (Corrector.finishControl(actualWrap,position)){
+                    topPanelIngr.setText("Congratulations, you did it!");
+                    position=0;
+                }else {
+                    topPanelIngr.setText("Paradajky");
+                    position++;
+                }
+            }else {
+                if (Corrector.isContains(actualWrap,"paradajky")){ // if its not correct then if wrap contain the ingredient
+                    topPanelIngr.setText("niečo si preskočil alebo tam toho dal trocha vela.. ale vratim  čas a dam ti este šancu..");
+                }else {
+                    topPanelIngr.setText("Ups.. pokazil si wrap, musiš ist znova!");
+                    position=0;
+                }
+            }
+        }
+    }
+
+    public void garnir2Rukola() {
+        if (Corrector.nullChecker(actualWrap)){ // check if its null
+            topPanelIngr.setText("Ups.. a aky že wrap to robime?");
+        }else {
+            if (Corrector.controlIngredient(actualWrap,position,"rukola")){ // check if its correct ingredient
+                if (Corrector.finishControl(actualWrap,position)){
+                    topPanelIngr.setText("Congratulations, you did it!");
+                    position=0;
+                }else {
+                    topPanelIngr.setText("Rukola");
+                    position++;
+                }
+            }else {
+                if (Corrector.isContains(actualWrap,"rukola")){ // if its not correct then if wrap contain the ingredient
+                    topPanelIngr.setText("niečo si preskočil alebo tam toho dal trocha vela.. ale vratim  čas a dam ti este šancu..");
+                }else {
+                    topPanelIngr.setText("Ups.. pokazil si wrap, musiš ist znova!");
+                    position=0;
+                }
+            }
+        }
+    }
+
+    public void garnir2Salat() {
+        if (Corrector.nullChecker(actualWrap)){ // check if its null
+            topPanelIngr.setText("Ups.. a aky že wrap to robime?");
+        }else {
+            if (Corrector.controlIngredient(actualWrap,position,"salat")){ // check if its correct ingredient
+                if (Corrector.finishControl(actualWrap,position)){
+                    topPanelIngr.setText("Congratulations, you did it!");
+                    position=0;
+                }else {
+                    topPanelIngr.setText("Salat");
+                    position++;
+                }
+            }else {
+                if (Corrector.isContains(actualWrap,"salat")){ // if its not correct then if wrap contain the ingredient
+                    topPanelIngr.setText("niečo si preskočil alebo tam toho dal trocha vela.. ale vratim  čas a dam ti este šancu..");
+                }else {
+                    topPanelIngr.setText("Ups.. pokazil si wrap, musiš ist znova!");
+                    position=0;
+                }
+            }
+        }
+    }
+
+    public void garnir2Uhorky() {
+        if (Corrector.nullChecker(actualWrap)){ // check if its null
+            topPanelIngr.setText("Ups.. a aky že wrap to robime?");
+        }else {
+            if (Corrector.controlIngredient(actualWrap,position,"uhorky")){ // check if its correct ingredient
+                if (Corrector.finishControl(actualWrap,position)){
+                    topPanelIngr.setText("Congratulations, you did it!");
+                    position=0;
+                }else {
+                    topPanelIngr.setText("Kysle Uhorky");
+                    position++;
+                }
+            }else {
+                if (Corrector.isContains(actualWrap,"uhorky")){ // if its not correct then if wrap contain the ingredient
+                    topPanelIngr.setText("niečo si preskočil alebo tam toho dal trocha vela.. ale vratim  čas a dam ti este šancu..");
+                }else {
+                    topPanelIngr.setText("Ups.. pokazil si wrap, musiš ist znova!");
+                    position=0;
+                }
+            }
+        }
+    }
+
+    // garnir 3
+    public void garnir3ChickenBurgSos() {
+        if (Corrector.nullChecker(actualWrap)){ // check if its null
+            topPanelIngr.setText("Ups.. a aky že wrap to robime?");
+        }else {
+            if (Corrector.controlIngredient(actualWrap,position,"chickBurgSos")){ // check if its correct ingredient
+                if (Corrector.finishControl(actualWrap,position)){
+                    topPanelIngr.setText("Congratulations, you did it!");
+                    position=0;
+                }else {
+                    topPanelIngr.setText("ChickenBurger Sos");
+                    position++;
+                }
+            }else {
+                if (Corrector.isContains(actualWrap,"chickBurgSos")){ // if its not correct then if wrap contain the ingredient
+                    topPanelIngr.setText("niečo si preskočil alebo tam toho dal trocha vela.. ale vratim  čas a dam ti este šancu..");
+                }else {
+                    topPanelIngr.setText("Ups.. pokazil si wrap, musiš ist znova!");
+                    position=0;
+                }
+            }
+        }
+    }
+
+    public void garnir3Kecup() {
+        if (Corrector.nullChecker(actualWrap)){ // check if its null
+            topPanelIngr.setText("Ups.. a aky že wrap to robime?");
+        }else {
+            if (Corrector.controlIngredient(actualWrap,position,"kecup")){ // check if its correct ingredient
+                if (Corrector.finishControl(actualWrap,position)){
+                    topPanelIngr.setText("Congratulations, you did it!");
+                    position=0;
+                }else {
+                    topPanelIngr.setText("Kečup");
+                    position++;
+                }
+            }else {
+                if (Corrector.isContains(actualWrap,"kecup")){ // if its not correct then if wrap contain the ingredient
+                    topPanelIngr.setText("niečo si preskočil alebo tam toho dal trocha vela.. ale vratim  čas a dam ti este šancu..");
+                }else {
+                    topPanelIngr.setText("Ups.. pokazil si wrap, musiš ist znova!");
+                    position=0;
+                }
+            }
+        }
+    }
+
+    public void garnir3Horcica() {
+        if (Corrector.nullChecker(actualWrap)){ // check if its null
+            topPanelIngr.setText("Ups.. a aky že wrap to robime?");
+        }else {
+            if (Corrector.controlIngredient(actualWrap,position,"horcica")){ // check if its correct ingredient
+                if (Corrector.finishControl(actualWrap,position)){
+                    topPanelIngr.setText("Congratulations, you did it!");
+                    position=0;
+                }else {
+                    topPanelIngr.setText("Horčica");
+                    position++;
+                }
+            }else {
+                if (Corrector.isContains(actualWrap,"horcica")){ // if its not correct then if wrap contain the ingredient
+                    topPanelIngr.setText("niečo si preskočil alebo tam toho dal trocha vela.. ale vratim  čas a dam ti este šancu..");
+                }else {
+                    topPanelIngr.setText("Ups.. pokazil si wrap, musiš ist znova!");
+                    position=0;
+                }
+            }
+        }
+    }
 }
