c9be14ff5843982d49acb0847f197a6bebd740ad
Aries023 <pet.potocka@gmail.com>
2022-09-19 13:43:27 +0200

    - add images (again) git forget them, so I added them with terminal - module-info.java => add requires javafx.media; - created class Sounds ( I will add some sound and music in future) - add dependency for media - add song Halycon - RunAway - add images in menu buttons (wraps)

---
+++ src/main/java/com/example/campan/stuffs/Sounds.java
@@ -0,0 +1,5 @@
+package com.example.campan.stuffs;
+
+public class Sounds {
+
+}
--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -2,6 +2,7 @@ module com.example.campan {
     requires javafx.controls;
     requires javafx.fxml;
     requires java.sql;
+    requires javafx.media;
 
 
     opens com.example.campan to javafx.fxml;
